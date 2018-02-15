e5a46270b003d93cacee7db5c97adc37f67cc596
Steven Lawson <unluckygamblermusic@gmail.com>
2018-02-15 12:12:42 -0700

    refactored insert

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -10,9 +10,7 @@ describe('Node', () => {
   it('should be a thing', () => {
     expect(node).to.exist
   })
-  // it('should be able to store words', () => {
-  //   expect(node.words).to.deep.equal([]);
-  // })
+  
   it('should be able to store child nodes', () => {
     expect(node.children).to.deep.equal({});
   })
