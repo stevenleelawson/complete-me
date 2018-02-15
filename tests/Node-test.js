d1218d6fc2215d4685a7686f2d4da545601fa630
Steven Lawson <unluckygamblermusic@gmail.com>
2018-02-15 12:19:11 -0700

    added test to node, got refactor passing tests

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -10,7 +10,9 @@ describe('Node', () => {
   it('should be a thing', () => {
     expect(node).to.exist
   })
-  
+  it('should have a completeWord property, assigned to zero', () => {
+    expect(node.completeWord).to.equal(0);
+  })
   it('should be able to store child nodes', () => {
     expect(node.children).to.deep.equal({});
   })
